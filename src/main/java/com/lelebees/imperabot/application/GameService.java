fef97daf8712b2533158c07e32fcbde4ecbce09a
Lelebees <Lelebees@protonmail.com>
2023-02-13 17:07:02 +0100

    remove checkGameForPing() because it's premature and poorly programmed as a result.

--- src/main/java/com/lelebees/imperabot/application/GameService.java
+++ src/main/java/com/lelebees/imperabot/application/GameService.java
@@ -16,23 +16,6 @@ public class GameService {
         this.gameRepository = gameRepository;
     }
 
-    //TODO: Implement Impera calls
-    public void checkGamesForPing()
-    {
-        List<Game> gameList = findAllGames();
-        for (Game game:gameList) {
-            // Check if it's someone's turn!
-            if (game.last_turn < 1)
-            {
-                //Do something!
-            }
-            if (game.half_time_notice == false && true)
-            {
-                //Do something when it's half-time!
-            }
-        }
-    }
-
     public Game findGameByID(int ID){
         Optional<Game> gameOptional = gameRepository.findById(ID);
         if (gameOptional.isPresent())
