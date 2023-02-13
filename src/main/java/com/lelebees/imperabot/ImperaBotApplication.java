a455370a79b67c55d3a9397f607960d96da725e1
Lelebees <Lelebees@protonmail.com>
2023-02-13 14:12:55 +0100

    project init

---
+++ src/main/java/com/lelebees/imperabot/ImperaBotApplication.java
@@ -0,0 +1,13 @@
+package com.lelebees.imperabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImperaBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImperaBotApplication.class, args);
+    }
+
+}
