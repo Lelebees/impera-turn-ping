e4fa68cf33cc97c58b9db3e2620b6d4e4e6c6834
Lelebees <Lelebees@protonmail.com>
2023-07-30 14:31:07 +0200

    Prepared User half of system to also no longer rely on switches for text output

--- src/main/java/com/lelebees/imperabot/bot/domain/user/UserNotificationSetting.java
+++ src/main/java/com/lelebees/imperabot/bot/domain/user/UserNotificationSetting.java
@@ -1,9 +1,20 @@
 package com.lelebees.imperabot.bot.domain.user;
 
 public enum UserNotificationSetting {
-    NO_NOTIFICATIONS,
-    DMS_ONLY,
-    GUILD_ONLY,
-    PREFER_GUILD_OVER_DMS,
-    DMS_AND_GUILD
+    NO_NOTIFICATIONS("No Notifications"),
+    DMS_ONLY("DMs Only"),
+    GUILD_ONLY("Guild Only"),
+    PREFER_GUILD_OVER_DMS("Guild Over DMs"),
+    DMS_AND_GUILD("DMs & Guild");
+
+    private final String textForm;
+
+    UserNotificationSetting(final String textForm) {
+        this.textForm = textForm;
+    }
+
+    @Override
+    public String toString() {
+        return this.textForm;
+    }
 }
