5066c32fc55983c4c5f0fe920cb2daf16835eeda
Lelebees <Lelebees@protonmail.com>
2023-10-26 00:25:33 +0200

    Fixed default channel always overriding specified channel

--- src/main/java/com/lelebees/imperabot/discord/domain/command/slash/TrackCommand.java
+++ src/main/java/com/lelebees/imperabot/discord/domain/command/slash/TrackCommand.java
@@ -80,7 +80,7 @@ public class TrackCommand implements SlashCommand {
             // We're in a guild, so track for guild
             Snowflake guildId = guildIdOptional.get();
             GuildSettings guildSettings = guildSettingsService.getOrCreateGuildSettings(guildId.asLong());
-            if (guildSettings.defaultChannelId != null) {
+            if (guildSettings.defaultChannelId != null && channelOptional.isEmpty()) {
                 channel = event.getInteraction().getGuild().block().getChannelById(Snowflake.of(guildSettings.defaultChannelId)).block();
                 logger.info("No channel was specified, but a default channel was set, and the command was used in a guild, so tracking in channel: " + channel.getId().asLong() + " (" + channel.getData().name().get() + ").");
             }
