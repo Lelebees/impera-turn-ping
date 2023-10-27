4130740a9c991919634da34d730c48eff101e2bd
Lelebees <Lelebees@protonmail.com>
2023-10-27 19:36:55 +0200

    changed motd

--- src/main/java/com/lelebees/imperabot/ImperaBotApplication.java
+++ src/main/java/com/lelebees/imperabot/ImperaBotApplication.java
@@ -36,7 +36,7 @@ public class ImperaBotApplication {
     public GatewayDiscordClient gatewayDiscordClient(@Value("${discord.token}") String discordToken) {
         return DiscordClientBuilder.create(discordToken).build()
                 .gateway()
-                .setInitialPresence(ignore -> ClientPresence.online(ClientActivity.watching("you make poor decisions")))
+                .setInitialPresence(ignore -> ClientPresence.online(ClientActivity.watching("empires rise and fall")))
                 .login()
                 .block();
     }
