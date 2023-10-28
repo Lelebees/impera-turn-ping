30468a76e71366fab69258717642866e1f4a4eee
Lelebees <Lelebees@protonmail.com>
2023-10-28 03:43:33 +0200

    Improved logging
    System is much more robust and informative

---
+++ src/main/java/com/lelebees/imperabot/bot/application/runnable/CheckTurns.java
@@ -0,0 +1,130 @@
+package com.lelebees.imperabot.bot.application.runnable;
+
+import com.lelebees.imperabot.bot.application.GameLinkService;
+import com.lelebees.imperabot.bot.application.GameService;
+import com.lelebees.imperabot.bot.domain.game.Game;
+import com.lelebees.imperabot.bot.domain.gamechannellink.GameChannelLink;
+import com.lelebees.imperabot.bot.domain.guild.GuildNotificationSettings;
+import com.lelebees.imperabot.discord.application.DiscordService;
+import com.lelebees.imperabot.impera.application.ImperaService;
+import com.lelebees.imperabot.impera.domain.game.view.ImperaGamePlayerDTO;
+import com.lelebees.imperabot.impera.domain.game.view.ImperaGameViewDTO;
+import discord4j.core.object.entity.channel.Channel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class CheckTurns implements Runnable {
+    private static final Logger logger = LoggerFactory.getLogger(CheckTurns.class);
+    private final ImperaService imperaService;
+    private final GameService gameService;
+    private final GameLinkService gameLinkService;
+    private final DiscordService discordService;
+
+    public CheckTurns(ImperaService imperaService, GameService gameService, GameLinkService gameLinkService, DiscordService discordService) {
+        this.imperaService = imperaService;
+        this.gameService = gameService;
+        this.gameLinkService = gameLinkService;
+        this.discordService = discordService;
+    }
+
+    @Override
+    public void run() {
+        try {
+            List<Game> trackedGames = gameService.findAllGames();
+            logger.info("[CT] Checking turns for " + trackedGames.size() + " games.");
+            int skippedGames = 0;
+            for (Game game : trackedGames) {
+                ImperaGameViewDTO imperaGame = imperaService.getGame(game.getId());
+
+                boolean gameEnded = imperaGame.state.equals("Ended");
+                boolean gameHasYetToStart = imperaGame.state.equals("Open");
+                boolean turnHasChanged = game.currentTurn != imperaGame.turnCounter;
+                boolean halfOfTurnTimeHasPassed = imperaGame.timeoutSecondsLeft <= (imperaGame.options.timeoutInSeconds / 2);
+                // If the turn hasn't changed, and it has not been half-time OR the notice has already been sent, and the game hasn't ended, or the game is open
+                if ((!turnHasChanged && (!halfOfTurnTimeHasPassed || game.halfTimeNotice) && !gameEnded) || gameHasYetToStart) {
+                    // Skip this game
+                    skippedGames++;
+                    continue;
+                }
+                // Get all players that have been defeated since the last turn change
+                List<ImperaGamePlayerDTO> defeatedPlayers = IntStream.range(game.currentTurn, imperaGame.turnCounter + 1)
+                        .mapToObj(turn -> imperaService.playersThatWereDefeated(game.getId(), turn))
+                        .flatMap(Collection::stream)
+                        .map(imperaGame::findPlayerByGameId)
+                        .toList();
+                logger.debug("Found " + defeatedPlayers.size() + " defeated players.");
+                // Get all players that surrendered in the last turn(s)
+                List<ImperaGamePlayerDTO> surrenderedPlayers = IntStream.range(game.currentTurn, imperaGame.turnCounter + 1)
+                        .mapToObj(turn -> imperaService.playersThatSurrendered(game.getId(), turn))
+                        .flatMap(Collection::stream)
+                        .map(imperaGame::findPlayerByGameId)
+                        .toList();
+                logger.debug("Found " + surrenderedPlayers.size() + " surrendered players.");
+
+                List<ImperaGamePlayerDTO> timedOutPlayers = IntStream.range(game.currentTurn, imperaGame.turnCounter + 1)
+                        .mapToObj(turn -> imperaService.playersThatTimedOut(game.getId(), turn))
+                        .flatMap(Collection::stream)
+                        .map(imperaGame::findPlayerByGameId)
+                        .toList();
+                logger.debug("Found " + timedOutPlayers.size() + " timed out players.");
+
+                // Get all channels that want to receive updates for this game.
+                List<Channel> channels = new ArrayList<>(gameLinkService.findLinksByGame(game.getId())
+                        .stream()
+                        .filter(gameChannelLink ->
+                                gameLinkService.deepGetNotificationSetting(gameChannelLink.getGameLinkId()) != GuildNotificationSettings.NO_NOTIFICATIONS)
+                        .map(GameChannelLink::getChannelId)
+                        .map(discordService::getChannelById)
+                        .toList());
+                logger.debug("Found " + channels.size() + " channels to notify.");
+
+                defeatedPlayers.forEach(player -> {
+                    logger.info("Sending defeated notice for " + imperaGame.name + " (" + imperaGame.id + ")!");
+                    discordService.sendDefeatedMessage(channels, player, imperaGame);
+                });
+                surrenderedPlayers.forEach(gamePlayer -> {
+                    logger.info("Sending surrendered notice for " + imperaGame.name + " (" + imperaGame.id + ")!");
+                    discordService.sendSurrenderMessage(channels, gamePlayer, imperaGame);
+                });
+                timedOutPlayers.forEach(gamePlayer -> {
+                    logger.info("Sending timed out notice for " + imperaGame.name + " (" + imperaGame.id + ")!");
+                    discordService.sendTimedOutMessage(channels, gamePlayer, imperaGame);
+                });
+                // There is probably a better way to do this, but I'm not sure what it is.
+                if (gameEnded) {
+                    logger.debug("Game " + game.getId() + " has ended!");
+                    logger.info("Sending victory notice for " + imperaGame.name + " (" + imperaGame.id + ")!");
+                    List<ImperaGamePlayerDTO> winningPlayers = imperaGame.teams.stream()
+                            .filter(team -> team.players.stream().anyMatch(player -> player.outcome.equals("Won")))
+                            .map(team -> team.players.stream()
+                                    .filter(player -> player.outcome.equals("Won"))
+                                    .toList())
+                            .flatMap(Collection::stream)
+                            .toList();
+                    // Send a message to all channels that are tracking this game, who won
+                    // TODO: make a proper winner message
+                    winningPlayers.forEach(winner -> discordService.sendVictorMessage(channels, winner, imperaGame));
+                    gameService.deleteGame(game.getId());
+                } else if (turnHasChanged) {
+                    //Send notice
+                    logger.info("Sending turn notice for " + imperaGame.name + " (" + imperaGame.id + ")!");
+                    discordService.sendNewTurnMessage(channels, imperaGame.currentPlayer, imperaGame);
+                    gameService.turnChanged(game.getId(), imperaGame.turnCounter);
+                } else {
+                    //Send half-time notice
+                    logger.info("Sending half time notice for " + imperaGame.name + " (" + imperaGame.id + ")!");
+                    discordService.sendHalfTimeMessage(channels, imperaGame.currentPlayer, imperaGame);
+                    gameService.setHalfTimeNoticeForGame(game.getId());
+                }
+            }
+            logger.info("Handled " + trackedGames.size() + " games, skipped " + skippedGames + " games.");
+        } catch (Exception e) {
+            logger.error("Handled Error: " + e.getMessage(), e);
+        }
+    }
+}
