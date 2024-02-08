485605b708a9603d1773851327472697627f42c9
Lelebees <Lelebees@protonmail.com>
2024-02-08 20:50:38 +0100

    Completely added ping command

--- src/main/java/com/lelebees/imperabot/discord/domain/command/slash/PingCommand.java
+++ src/main/java/com/lelebees/imperabot/discord/domain/command/slash/PingCommand.java
@@ -1,27 +1,22 @@
 package com.lelebees.imperabot.discord.domain.command.slash;
 
-import com.lelebees.imperabot.bot.application.UserService;
-import com.lelebees.imperabot.bot.domain.user.BotUser;
-import com.lelebees.imperabot.discord.application.DiscordService;
 import com.lelebees.imperabot.discord.domain.command.SlashCommand;
-import discord4j.common.util.Snowflake;
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
-import discord4j.core.object.component.ActionRow;
-import discord4j.core.object.component.Button;
+import discord4j.core.object.entity.User;
+import discord4j.core.spec.InteractionCallbackSpecDeferReplyMono;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
+
+import java.time.Instant;
 
 @Component
 public class PingCommand implements SlashCommand {
 
     private final static Logger logger = LoggerFactory.getLogger(PingCommand.class);
-    private final DiscordService discordService;
 
-    public PingCommand(DiscordService discordService) {
-        this.discordService = discordService;
+    public PingCommand() {
     }
 
     @Override
@@ -31,7 +26,13 @@ public class PingCommand implements SlashCommand {
 
     @Override
     public Mono<Void> handle(ChatInputInteractionEvent event) {
-        return event.reply()
-                .withContent("Pong!");
+        InteractionCallbackSpecDeferReplyMono spec = event.deferReply();
+        Instant requestFired = event.getCommandId().getTimestamp();
+        long difference = Instant.now().toEpochMilli() - requestFired.toEpochMilli();
+        float differenceInMilis = (float) difference / 10000;
+        User user = event.getInteraction().getUser();
+        logger.info(user.getUsername() + " (" + user.getId().asLong() + ") used /ping!");
+        return spec.event().reply()
+                .withContent("Pong in " + differenceInMilis + "ms");
     }
 }
